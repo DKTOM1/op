6f9c94ac7dea18b68ca67641fc9438ca30bc877f
dongkang1993 <2741896659@qq.com>
2017-11-25 23:52:23 +0800

    jggg

---
+++ dk/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from  django.http import HttpResponse
+from django.shortcuts import render, loader
+
+
+# Create your views here.
+def homePage(request):
+    temp = loader.get_template('wx.html')
+    conx = {'name': 'dk', 'age': 24, 'sex': 'M', 'home': 'FuYang'}
+    return HttpResponse(render(request, 'wx.html', conx))
